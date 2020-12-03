4103ffbe9397c53595dd16dd7d5cb7c9ff47c375
jair <hybrid.heroj@gmail.com>
2020-12-03 13:57:39 -0600

    reload

--- vistas/paginas/registro.php
+++ vistas/paginas/registro.php
@@ -69,7 +69,17 @@ $plantilla -> ctrTraerPlantilla();
 		$registro = ControladorFormularios::ctrRegistro();
 		if($registro == "ok"){
 
-			echo '<div class="alert alert-success">El administrador ha sido registrado </div>';
+			
+			echo '<script>
+			if(window.history.replaceState){
+				
+				window.history.replaceState(null , null, window.location.href);
+			}
+			
+
+			</script>';
+			
+			echo '<div class="alert alert-succes">El administrador ha sido registrado </div>';
 		}
 
 		?>
